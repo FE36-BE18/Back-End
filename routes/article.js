463764a3a37ac19709bdebeee76c123f9a0889b6
salimsulaiman <salimsulaiman75@gmail.com>
2023-06-14 12:49:31 +0700

    add endpoint get article by id

--- routes/article.js
+++ routes/article.js
@@ -4,20 +4,33 @@ const router = express.Router();
 const Article = require("../models/Article");
 
 router.get("/", async (req, res) => {
-    try {
-        const data = await Article.find({});
-        res.status(200).json(data);
-    } catch (error) {
-        res.status(400).json({
-            status: res.statusCode,
-            message: "Error",
-        });
+  try {
+    const data = await Article.find({});
+    res.status(200).json(data);
+  } catch (error) {
+    res.status(400).json({
+      status: res.statusCode,
+      message: "Error",
+    });
+  }
+});
+
+router.get("/:id", async (req, res) => {
+  try {
+    const data = await Article.findById(req.params.id);
+    if (!data) {
+      return res.status(404).json({
+        status: res.statusCode,
+        message: "Article not found",
+      });
     }
-})
-
-
-
-
-
+    res.status(200).json(data);
+  } catch (error) {
+    res.status(400).json({
+      status: res.statusCode,
+      message: "Error",
+    });
+  }
+});
 
 module.exports = router;
