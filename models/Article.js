babbfa934542cd63567aef29c2c572dd240eae5a
salimsulaiman <salimsulaiman75@gmail.com>
2023-06-14 12:36:46 +0700

    add desc field article

--- models/Article.js
+++ models/Article.js
@@ -1,19 +1,23 @@
 const mongoose = require("mongoose");
 
 const articleSchema = mongoose.Schema({
-    title: {
-        type: String,
-        required: true,
-        max: 255,
-    },
-    date: {
-        type: Date,
-        required: true,
-    },
-    content: {
-        type: String,
-        required: true,
-    },
+  title: {
+    type: String,
+    required: true,
+    max: 255,
+  },
+  date: {
+    type: Date,
+    required: true,
+  },
+  content: {
+    type: String,
+    required: true,
+  },
+  desc: {
+    type: String,
+    required: true,
+  },
 });
 
 module.exports = mongoose.model("Article", articleSchema);
